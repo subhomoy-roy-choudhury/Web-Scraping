7a63de54d0654856a18f3bf22fe66f6eae58211b
Subhomoy Roy Choudhury <64763636+subhomoy-roy-choudhury@users.noreply.github.com>
2021-06-16 11:27:23 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests 
-
-# https://your-heroku-app-name.herokuapp.com/predict
-# http://localhost:5000/predict
-resp = requests.post("http://localhost:5000/research_paper/", params={'keyword': 'machine learning'})
-# resp = requests.post("http://localhost:5000/google_news/")
-
-print(resp.text)
